25b18060317267e60e48c085410f3d0799311f2b
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2025-04-30 03:23:23 +0100

    render update

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
